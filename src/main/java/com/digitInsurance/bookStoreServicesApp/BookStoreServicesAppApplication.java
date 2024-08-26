6f51ab16fce9007d4e36fa88a50044a2d677360e
Ashish 1 <Ashish.1@godigit.com>
2024-08-26 17:39:17 +0530

    [Ashish] ADD: Created project structure.

--- src/main/java/com/digitInsurance/bookStoreServicesApp/BookStoreServicesAppApplication.java
+++ src/main/java/com/digitInsurance/bookStoreServicesApp/BookStoreServicesAppApplication.java
@@ -1,4 +1,4 @@
-package com.workshop.bookStoreServicesApp;
+package com.digitInsurance.bookStoreServicesApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/digitInsurance/bookStoreServicesApp/BookStoreServicesAppApplicationTests.java
+++ src/test/java/com/digitInsurance/bookStoreServicesApp/BookStoreServicesAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.workshop.bookStoreServicesApp;
+package com.digitInsurance.bookStoreServicesApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
