9212a4caf81f3fb034357fa86329cd45d661b984
Prathamesh Mahesh <Prathamesh.Mahesh@godigit.com>
2024-08-28 12:49:01 +0530

    [Aradhya] ADD: Creted the UserService Class

--- src/main/java/com/digitInsurance/bookStoreServicesApp/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/digitInsurance/bookStoreServicesApp/service/serviceImpl/UserServiceImpl.java
@@ -1,4 +1,4 @@
 package com.digitInsurance.bookStoreServicesApp.service.serviceImpl;
 
-public class UserService {
+public class UserServiceImpl {
 }
---
+++ src/main/java/com/digitInsurance/bookStoreServicesApp/service/serviceInterfaces/UserService.java
@@ -0,0 +1,4 @@
+package com.digitInsurance.bookStoreServicesApp.service.serviceInterfaces;
+
+public interface UserService {
+}
