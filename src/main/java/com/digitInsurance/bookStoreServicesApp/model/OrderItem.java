5e4356a7f7f9c52232b2da330a6c7e2d886f9f60
Ashish 1 <Ashish.1@godigit.com>
2024-08-29 12:19:50 +0530

    [Ashish] ADD: created the OrderItem entity

---
+++ src/main/java/com/digitInsurance/bookStoreServicesApp/model/OrderItem.java
@@ -0,0 +1,30 @@
+package com.digitInsurance.bookStoreServicesApp.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "order_item")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    @JsonIgnore
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private BookStore book;
+
+    private Integer quantity;
+    private Double price;
+}
