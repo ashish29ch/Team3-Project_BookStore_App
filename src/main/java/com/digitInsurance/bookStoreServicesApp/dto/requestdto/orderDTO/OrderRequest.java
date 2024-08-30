780c952965c63993b95d427be93628476c5c59b5
Prathamesh Mahesh <Prathamesh.Mahesh@godigit.com>
2024-08-30 23:22:55 +0530

    [Prathamesh] Add : Created the Order DTO for responce

--- src/main/java/com/digitInsurance/bookStoreServicesApp/dto/requestdto/orderDTO/OrderRequest.java
+++ src/main/java/com/digitInsurance/bookStoreServicesApp/dto/requestdto/orderDTO/OrderRequest.java
@@ -1,18 +1,14 @@
 package com.digitInsurance.bookStoreServicesApp.dto.requestdto.orderDTO;
 
 import com.digitInsurance.bookStoreServicesApp.model.PaymentDetails;
-import com.digitInsurance.bookStoreServicesApp.model.ShippingDetails;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderRequest {
-    //private ShippingDetails shippingDetails;
     private PaymentDetails paymentDetails;
 }
 
