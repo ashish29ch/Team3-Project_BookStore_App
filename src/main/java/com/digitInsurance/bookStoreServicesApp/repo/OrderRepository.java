31b6be85a16b6ae8e0f33fbc5ea614df2dcf7883
Prathamesh Mahesh <Prathamesh.Mahesh@godigit.com>
2024-08-30 23:17:30 +0530

    [Prathamesh] ADD : Implimented the Order Repository

--- src/main/java/com/digitInsurance/bookStoreServicesApp/repo/OrderRepository.java
+++ src/main/java/com/digitInsurance/bookStoreServicesApp/repo/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.digitInsurance.bookStoreServicesApp.repo;
 
+import com.digitInsurance.bookStoreServicesApp.model.BookStore;
 import com.digitInsurance.bookStoreServicesApp.model.Order;
 import com.digitInsurance.bookStoreServicesApp.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,8 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
     List<Order> findByUser(Users user);
+
+    boolean existsByUserAndItemsBook(Users user, BookStore book);
+
+    List<Order> findByUserId(Long id);
 }
