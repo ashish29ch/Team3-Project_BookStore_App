2eb37d0dba9adeeb452599eee6e659621fa5534c
Prathamesh Mahesh <Prathamesh.Mahesh@godigit.com>
2024-08-30 10:33:12 +0530

    [Prathamesh] Add : Created the orderservice interface

--- src/main/java/com/digitInsurance/bookStoreServicesApp/service/serviceInterfaces/OrderService.java
+++ src/main/java/com/digitInsurance/bookStoreServicesApp/service/serviceInterfaces/OrderService.java
@@ -1,8 +1,12 @@
 package com.digitInsurance.bookStoreServicesApp.service.serviceInterfaces;
 
 import com.digitInsurance.bookStoreServicesApp.dto.requestdto.orderDTO.OrderRequest;
+import com.digitInsurance.bookStoreServicesApp.exception.customException.ResourceNotFoundException;
 import com.digitInsurance.bookStoreServicesApp.model.Order;
 
+import java.util.List;
+
 public interface OrderService {
-    public Order placeOrder(OrderRequest orderRequest);
+    public Order placeOrder(Long userId, OrderRequest orderRequest) throws ResourceNotFoundException;
+    public List<Order> getOrderHistory(Long userId) throws ResourceNotFoundException;
 }
